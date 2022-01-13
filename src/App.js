ce11b3a420e53495740ebbc74e719d825d1da900
Nahidjc <nahidjc80@gmail.com>
2022-01-13 12:13:23 +0600

    link change

--- src/App.js
+++ src/App.js
@@ -3,18 +3,28 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import HomeScreen from "./screens/HomeScreen";
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import ProductScreen from "./screens/ProductScreen";
 
 function App() {
   return (
-    <div >
+    <Router>
       <Header></Header>
       <main className="py-3">
         <Container>
-          <HomeScreen></HomeScreen>
+          <Routes >
+            <Route path='/' element={<HomeScreen />} exact />
+            <Route path='/product/:id' element={<ProductScreen />} exact />
+          </Routes>
+
         </Container>
       </main>
       <Footer></Footer>
-    </div>
+    </Router>
   );
 }
 

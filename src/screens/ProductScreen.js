caac82e02160148ac48b4651273a666434c1c25f
Nahidjc <nahidjc80@gmail.com>
2022-01-14 01:00:09 +0600

    single product show

--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -1,9 +1,71 @@
 import React from 'react';
+import { Button, Card, Col, Image, ListGroup, Row } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
+import Rating from '../components/Rating';
+import products from '../products';
 
-const ProductScreen = () => {
+const ProductScreen = ({ match }) => {
+    let params = useParams();
+    console.log(match);
+    const product = products.find(p => p._id == params.id)
+    console.log(product);
     return (
         <div>
-            Product
+            <Link to='/' className='btn btn-light my-3'><i class="fas fa-angle-double-left"></i> Go Back</Link>
+            <Row>
+                <Col md={6}>
+                    <Image src={product.image} alt={product.name} fluid />
+                </Col>
+                <Col md={3}>
+                    <ListGroup variant='flush'>
+                        <ListGroup.Item>
+                            <h3>{product.name}</h3>
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            <Rating value={product.rating} text={`${product.numReviews} reviews`} color={"#f78205"} />
+                        </ListGroup.Item>
+
+                        <ListGroup.Item>
+                            Price: ${product.price}
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            Description: {product.description}
+                        </ListGroup.Item>
+                    </ListGroup>
+                </Col>
+                <Col md={3}>
+                    <Card>
+                        <ListGroup variant='flush'>
+                            <ListGroup.Item>
+                                <Row>
+                                    <Col>Price:</Col>
+                                    <Col>
+                                        <strong>${product.price}</strong>
+                                    </Col>
+                                </Row>
+                            </ListGroup.Item>
+
+
+                            <ListGroup.Item>
+                                <Row>
+                                    <Col>Status:</Col>
+                                    <Col>
+                                        {product.countInStock > 0 ? 'In Stock' : 'Out of Stock'}
+                                    </Col>
+                                </Row>
+                            </ListGroup.Item>
+
+                            <ListGroup.Item>
+                                <div className='d-flex justify-content-center'>
+                                    <Button className='btn-block ' disabled={product.countInStock == 0} type='button'>Add to Cart</Button>
+                                </div>
+
+                            </ListGroup.Item>
+                        </ListGroup>
+                    </Card>
+                </Col>
+            </Row>
         </div>
     );
 };

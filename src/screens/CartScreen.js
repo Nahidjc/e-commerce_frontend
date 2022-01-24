b2192fa4bdea65728207f2bc4353dea012af771f
Nahidjc <nahidjc80@gmail.com>
2022-01-25 00:44:14 +0600

    cart save in redux

--- src/screens/CartScreen.js
+++ src/screens/CartScreen.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useLocation, useNavigate, useParams } from 'react-router-dom';
+import { addToCart } from '../actions/cartActions';
 
 const CartScreen = () => {
-    console.log("Cart");
+    let location = useLocation();
+    let params = useParams();
+    const dispatch = useDispatch();
+    // const history = useNavigate();
+    const productId = params.id;
+    const qty = location.search ? Number(location.search.split('=')[1]) : 1
+    console.log("Cart", qty);
+    const cart = useSelector(state => state.cart);
+    const { cartItems } = cart;
+    console.log(cartItems);
+    useEffect(() => {
+        if (productId) {
+            dispatch(addToCart(productId, qty))
+        }
+    }, [dispatch, productId, qty])
     return (
         <div>
             cart
